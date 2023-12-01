84285061ec56d1b18210524fda21942aef2b231c
ccameron2 <74011941+ccameron2@users.noreply.github.com>
2023-12-01 17:02:19 +0000

    --

--- Day1_2023/source.cpp
+++ Day1_2023/source.cpp
@@ -14,9 +14,9 @@ int main()
 
     while (std::getline(inFile, inString))
     {
+        string alphabeticalNumber = "";
         for (string::iterator it = inString.begin(); it != inString.end(); ++it)
         {
-            string alphabeticalNumber = "";
             if (std::isdigit(*it))
             {
                 twoDigitNum += *it;
@@ -24,7 +24,10 @@ int main()
             }
             else
             {
-                alphabeticalNumber.append(&*it);
+                string str = " ";
+                str[0] = *it;
+                alphabeticalNumber.append(str);
+                auto a = inString.find("nine");
             }
         }
         for (string::iterator it = inString.end(); it != inString.begin() - 1; --it)
@@ -45,48 +48,4 @@ int main()
     system("pause");
 
     return 0;
-}
-
-int isAlphabeticalNumber(string input)
-{
-    if (input == "one")
-    {
-        return 1;
-    }
-    else if (input == "two")
-    {
-        return 2;
-    }
-    else if (input == "three")
-    {
-        return 3;
-    }
-    else if (input == "four")
-    {
-        return 4;
-    }
-    else if (input == "five")
-    {
-        return 5;
-    }
-    else if (input == "six")
-    {
-        return 6;
-    }
-    else if (input == "seven")
-    {
-        return 7;
-    }
-    else if (input == "eight")
-    {
-        return 8;
-    }
-    else if (input == "nine")
-    {
-        return 9;
-    }
-    else
-    {
-        return 0;
-    }
 }
