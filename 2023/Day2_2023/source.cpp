43b9bc8a56fc4733c3bb5e89e72fc611a698b6f3
Callum Cameron <ccameron2@outlook.com>
2023-12-04 21:37:20 +0000

    more day 2

--- 2023/Day2_2023/source.cpp
+++ 2023/Day2_2023/source.cpp
@@ -13,45 +13,62 @@ int main()
     string inString;
     std::ifstream inFile("inputVal.txt");
     int idSum = 0;
+    int id = 0;
 
     while(inFile >> inString)
     {
+        bool possible = true;
         if(inString == "Game")
         {
             // Get ID
             inFile >> inString;
-            idSum += charToInt(inString[0]);
+            id = charToInt(inString[0]);
             
             char backChar = ' ';
+            string backStr = " ";
+            backStr[0] = backChar;
 
             int r = 0, g = 0, b = 0;
+            while(backStr != "\n")
+            {
+                while(backChar != ';')
+                {        
+                    inFile >> inString;
 
-            while(backChar != ';')
-            {        
-                inFile >> inString;
+                    int numCubes = std::stoi(inString);
 
-                int numCubes = std::stoi(inString);
-
-                inFile >> inString;
+                    inFile >> inString;
                
-                backChar = inString.back(); 
-                inString.pop_back();
+                    backChar = inString.back(); 
+                    inString.pop_back();
 
-                if(inString == "red")
-                {
-                    r += numCubes;
+                    if(inString == "red")
+                    {
+                        r += numCubes;
+                    }
+                    else if(inString == "green")
+                    {
+                        g += numCubes;
+                    }
+                     else if(inString == "blue")
+                    {
+                        b += numCubes;
+                    }
                 }
-                else if(inString == "green")
-                {
-                    g += numCubes;
-                }
-                else if(inString == "blue")
-                {
-                    b += numCubes;
-                }
+
+                // Check if game is possible then reset
+                if      (r > 12) possible = false; 
+                else if (g > 13) possible = false; 
+                else if( b > 14) possible = false;
+                r = 0, g = 0, b = 0;
             }
-            
+            if(possible)
+            {
+                idSum += charToInt(inString[0]);
+                possible = true;
+            }                     
         }
+        
     }
     std::cout << "Sum: " << idSum << std::endl;
     system("pause");
