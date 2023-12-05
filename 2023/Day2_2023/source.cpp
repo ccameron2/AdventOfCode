a8009a4f4f7f1521143117cef88770856a3c0311
ccameron2 <74011941+ccameron2@users.noreply.github.com>
2023-12-05 12:41:17 +0000

    more day 2 parsing

--- 2023/Day2_2023/source.cpp
+++ 2023/Day2_2023/source.cpp
@@ -4,7 +4,8 @@
 
 using std::string;
 
-int charToInt(char ch){
+int charToInt(char ch)
+{
     return ch - '0';
 }
 
@@ -15,60 +16,76 @@ int main()
     int idSum = 0;
     int id = 0;
 
-    while(inFile >> inString)
+    while (inFile >> inString)
     {
-        bool possible = true;
-        if(inString == "Game")
+        if (inString == "Game")
         {
+            bool possible = true;
+
             // Get ID
             inFile >> inString;
             id = charToInt(inString[0]);
-            
+
             char backChar = ' ';
-            string backStr = " ";
-            backStr[0] = backChar;
 
+            bool endOfLine = false;
             int r = 0, g = 0, b = 0;
-            while(backStr != "\n")
+            while (!endOfLine)
             {
-                while(backChar != ';')
-                {        
+                // Loop through cube sets
+                while (backChar != ';' && !endOfLine)
+                {
+                    // Read number of cubes
                     inFile >> inString;
-
                     int numCubes = std::stoi(inString);
 
+                    // Read colour of cubes
                     inFile >> inString;
-               
-                    backChar = inString.back(); 
-                    inString.pop_back();
+                    backChar = inString.back();
 
-                    if(inString == "red")
+                    // Check back of string for separators
+                    if (backChar == ';' || backChar == ',')
+                    {
+                        inString.pop_back();
+                    }
+                    else
+                    {
+                        endOfLine = true;
+                    }
+
+                    // Assign number to colour of cubes
+                    if (inString == "red")
                     {
                         r += numCubes;
                     }
-                    else if(inString == "green")
+                    else if (inString == "green")
                     {
                         g += numCubes;
                     }
-                     else if(inString == "blue")
+                    else if (inString == "blue")
                     {
                         b += numCubes;
                     }
                 }
 
                 // Check if game is possible then reset
-                if      (r > 12) possible = false; 
-                else if (g > 13) possible = false; 
-                else if( b > 14) possible = false;
+                if (r > 12)
+                    possible = false;
+                else if (g > 13)
+                    possible = false;
+                else if (b > 14)
+                    possible = false;
                 r = 0, g = 0, b = 0;
+                backChar = ' ';
             }
-            if(possible)
+
+            // Add id if game is possible
+            if (possible)
             {
                 idSum += charToInt(inString[0]);
                 possible = true;
-            }                     
+            }
         }
-        
     }
     std::cout << "Sum: " << idSum << std::endl;
     system("pause");
