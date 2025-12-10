e3df9c4e44142e39e5d1d15d51199bef49cc1afa
ccameron2 <74011941+ccameron2@users.noreply.github.com>
2025-12-10 20:20:22 +0000

    day 2 complete

--- 2025/AoC2025/src/Day2/Day2.cpp
+++ 2025/AoC2025/src/Day2/Day2.cpp
@@ -1,15 +1,58 @@
 ﻿#include "Day2.h"
 
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
+
+bool CompareNumVecs(std::vector<int> a, std::vector<int> b)
+{
+	if (a.size() != b.size())
+	{
+		return false;
+	}
+	
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (a[i] != b[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+// returns true if repeating
+bool CheckDigits(int numDigits, std::vector<int>& number)
+{
+	int numTimesCanContain = number.size() / numDigits;
+	
+	std::vector<std::vector<int>> combinations;
+	for (int i = 0; i < numTimesCanContain; i++)
+	{
+		combinations.push_back(std::vector<int>());
+		for (int j = 0; j < numDigits; j++)
+		{
+			combinations[i].push_back(number[i * numDigits + j]);
+		}
+	}
+	
+	for (auto& combination : combinations)
+	{
+		if (!CompareNumVecs(combination, combinations[0]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
 
 Day2::Day2()
 {
 	std::vector<std::string> InputStrings;
 	
-	std::ifstream file("example2.txt");
+	std::ifstream file("input2.txt");
 
 	if (file.is_open()) 
 	{
@@ -30,16 +73,79 @@ Day2::Day2()
 	for (auto& str : InputStrings)
 	{
 		std::stringstream ss(str);
-		std::string segment;
-		std::getline(ss, segment, '-');
-		long long low = std::stoll(segment);
-		std::getline(ss, segment);
-		long long high = std::stoll(segment);
-		Ranges.push_back(Range(low, high));
+		std::string lowSegment;
+		std::string highSegment;
+		std::getline(ss, lowSegment, '-');
+		std::getline(ss, highSegment);
+		Ranges.emplace_back(Range(lowSegment, highSegment));
 	}
 	
+	std::vector<uint64_t> invalidIDs;
 	for (auto& range : Ranges)
 	{
-		range.Count = range.High - range.Low;
+		for (auto& number : range.Numbers)
+		{
+			uint64_t count = number.size();
+			uint64_t maxRep = count / 2;
+			
+			if (false)
+			{
+				std::vector<int> testVec1;
+				for (int i = 0; i < maxRep; i++)
+				{
+					testVec1.push_back(number[i]);
+				}
+			
+				std::vector<int> testVec2;
+				for (int i = maxRep; i < count; i++)
+				{
+					testVec2.push_back(number[i]);
+				}
+			
+				if (CompareNumVecs(testVec1, testVec2))
+				{
+					std::ostringstream os;
+					for (auto& num : number)
+					{
+						os << num;
+					}
+					invalidIDs.push_back(std::stoll(os.str()));	
+				}
+			}
+			
+			bool valid = true;
+			for (int i = maxRep; i > 0; i--)
+			{
+				if (number.size() % i != 0)
+				{
+					continue;
+				}
+				if (CheckDigits(i, number))
+				{
+					valid = false;
+					break;
+				}
+			}
+			
+			if (valid == false)
+			{
+				std::ostringstream os;
+				for (auto& num : number)
+				{
+					os << num;
+				}
+				uint64_t id = std::stoll(os.str());
+				invalidIDs.push_back(id);	
+			}
+		}
 	}
+	
+	uint64_t result = 0;
+	for (auto& id : invalidIDs)
+	{
+		result += id;
+	}
+	
+	std::cout << "Result: " << result << std::endl;
+	
 }
--- 2025/AoC2025/src/Day2/Day2.h
+++ 2025/AoC2025/src/Day2/Day2.h
@@ -1,15 +1,37 @@
 ﻿#pragma once
+#include <locale>
+#include <sstream>
+#include <string>
+#include <vector>
+
 #include "Util.h"
 
 struct Range
 {
-	Range(int low, int high) : Low(low), High(high)
+	Range(std::string low, std::string high) : Low(low), High(high)
 	{
-		Count = high - low;
+		Count = std::stoll(high) - std::stoll(low);
+		Numbers.reserve(Count);
+		for (uint64_t i = 0; i <= Count; i++)
+		{
+			uint64_t number = std::stoll(low) + i;
+			std::ostringstream os;
+
+			os << number;
+			std::string digits = os.str();
+			std::vector<int> numberVec;
+			for (auto& ch : digits)
+			{
+				numberVec.push_back(ch - '0');
+			}
+			
+			Numbers.push_back(numberVec);
+		}
 	}
-	int Low;
-	int High;
-	int Count = 0;
+	std::string Low;
+	std::string High;
+	uint64_t Count = 0;
+	std::vector<std::vector<int>> Numbers;
 };
 
 class Day2 : public Day
